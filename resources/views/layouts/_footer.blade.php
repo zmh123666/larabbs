f73de94d8075a14eb958d03ed613d430afda6d6b
Nickel <a_shy_sky@163.com>
2018-11-05 06:48:59 +0000

    Finished basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/3352963624/profile?rightmod=1&wvr=6&mod=personnumber" target="_blank">Nickel</a> 编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="pull-right"><a href="mailto:a_shy_sky@163.com">联系我们</a></p>
+
+    </div>
+</footer>
