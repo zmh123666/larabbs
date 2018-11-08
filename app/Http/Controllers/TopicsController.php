2abc2e8717bd187663526d124da44766326127fc
Nickel <a_shy_sky@163.com>
2018-11-08 03:12:52 +0000

    List topics created by user

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,7 @@ class TopicsController extends Controller
 
 	public function index(Request $request, Topic $topic)
 	{
-		$topics = $topic->withOrder($request->order)->paginate(20)
+		$topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
